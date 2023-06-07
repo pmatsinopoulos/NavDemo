e5bda3195b76d7c6f54f2687fcb4e32ed2234917
Panos Matsinopoulos <panos@mixlr.com>
2023-06-07 15:07:39 +0300

    Added new fragment with a text and submit button. This uses view binding too.

---
+++ app/src/main/java/com/mixlr/panos/navdemo/HomeFragment.kt
@@ -0,0 +1,24 @@
+package com.mixlr.panos.navdemo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mixlr.panos.navdemo.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
