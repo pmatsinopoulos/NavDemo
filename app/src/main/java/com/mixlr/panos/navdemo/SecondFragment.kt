7672a1d3ba427c4da256635a678ad1e282971d14
Panos Matsinopoulos <panos@mixlr.com>
2023-06-07 15:15:24 +0300

    Added second fragment and include view binding to it.

---
+++ app/src/main/java/com/mixlr/panos/navdemo/SecondFragment.kt
@@ -0,0 +1,24 @@
+package com.mixlr.panos.navdemo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mixlr.panos.navdemo.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+    private lateinit var binding: FragmentSecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
