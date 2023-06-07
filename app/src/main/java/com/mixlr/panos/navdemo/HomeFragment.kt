7feef1a040f969fdfd63d84bb0c0dccae925d5a8
Panos Matsinopoulos <panos@mixlr.com>
2023-06-07 15:27:53 +0300

    Do not allow passing blank data from homeFragment to secondFragment

--- app/src/main/java/com/mixlr/panos/navdemo/HomeFragment.kt
+++ app/src/main/java/com/mixlr/panos/navdemo/HomeFragment.kt
@@ -1,10 +1,12 @@
 package com.mixlr.panos.navdemo
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.os.bundleOf
 import androidx.navigation.findNavController
 import com.mixlr.panos.navdemo.databinding.FragmentHomeBinding
@@ -22,9 +24,13 @@ class HomeFragment : Fragment() {
     ): View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         binding.btnSubmit.setOnClickListener {
-            val bundle = bundleOf("user_input" to binding.etName.text.toString())
+            if (!TextUtils.isEmpty(binding.etName.text.toString())) {
+                val bundle = bundleOf("user_input" to binding.etName.text.toString())
 
-            it.findNavController().navigate(R.id.action_homeFragment_to_secondFragment, bundle)
+                it.findNavController().navigate(R.id.action_homeFragment_to_secondFragment, bundle)
+            } else {
+                Toast.makeText( activity, "Please enter a name", Toast.LENGTH_LONG).show()
+            }
         }
         return binding.root
     }
