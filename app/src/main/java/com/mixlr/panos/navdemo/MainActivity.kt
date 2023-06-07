c4fb0b544d8b8a26534ee506ed77c10e42b53d35
Panos Matsinopoulos <panos@mixlr.com>
2023-06-07 13:56:42 +0300

    Added 'binding' to the MainActivity

--- app/src/main/java/com/mixlr/panos/navdemo/MainActivity.kt
+++ app/src/main/java/com/mixlr/panos/navdemo/MainActivity.kt
@@ -2,10 +2,14 @@ package com.mixlr.panos.navdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mixlr.panos.navdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
